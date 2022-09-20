1d564b468cd210bf86a2b49c41abb7e8f2c55f41
EC2 Default User <ec2-user@ip-172-31-43-22.ap-northeast-1.compute.internal>
2022-09-20 07:45:36 +0000

    [Add]会員登録情報と退会ボタン

--- app/controllers/public/customers_controller.rb
+++ app/controllers/public/customers_controller.rb
@@ -1,16 +1,27 @@
 class Public::CustomersController < ApplicationController
   def show
+    @customer = current_customer
   end
 
   def edit
+    @customer = current_customer
   end
 
   def update
+    customer = current_customer
+    customer.update(customer_params)
+    redirect_to public_customers_path
   end
 
   def confirm
   end
 
   def destroy
+  end
+
+  private
+
+  def customer_params
+    params.require(:customer).permit(:first_name, :last_name, :first_name_kana, :last_name_kana, :email, :postcode, :address, :phone_number)
   end
 end
--- app/models/customer.rb
+++ app/models/customer.rb
@@ -8,10 +8,7 @@ class Customer < ApplicationRecord
   validates :last_name, presence: true
   validates :first_name_kana, presence: true
   validates :last_name_kana, presence: true
-  validates :email, presence: true
   validates :postcode, presence: true
   validates :address, presence: true
   validates :phone_number, presence: true
-  validates :password, presence: true
-  validates :password_confirmation, presence: true
 end
