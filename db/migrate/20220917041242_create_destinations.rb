91fca7760cb77215a873686d2a7d03a805036252
yooske <yooske0214@gmail.com>
2022-09-20 10:02:02 +0000

    配送先 登録・編集・削除機能の作成、index,editページの作成

--- db/migrate/20220917041242_create_destinations.rb
+++ db/migrate/20220917041242_create_destinations.rb
@@ -1,7 +1,7 @@
 class CreateDestinations < ActiveRecord::Migration[6.1]
   def change
     create_table :destinations do |t|
-      
+
       t.integer :customer_id, null: false
       t.string :shipping_postcode, null: false
       t.string :shipping_address, null: false
