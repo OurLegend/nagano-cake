e91130d667a6aba20da7dcdf2a4d85c4fd4b67e9
EC2 Default User <ec2-user@ip-172-31-35-7.ap-northeast-1.compute.internal>
2022-09-21 08:27:26 +0000

    adomin-customers途中

--- app/controllers/admin/customers_controller.rb
+++ app/controllers/admin/customers_controller.rb
@@ -1,13 +1,22 @@
 class Admin::CustomersController < ApplicationController
   def index
+    @customers = Customer.all
   end
 
   def show
+    @customer = Customer.find(params[:id])
   end
 
   def edit
+    @customer = Customer.find(params[:id])
   end
 
   def update
   end
+  
+  private
+  
+  def customer_params
+    params.require(:customer).permit(:id, :first_name, :last_name, :first_name_kana, :last_name_kana, :postcode, :address, :is_deleted, :phone_number)
+  end  
 end
