e1bc9a539b79c7fd1aedd51bea5311be6f3f735f
EC2 Default User <ec2-user@ip-172-31-17-129.ap-northeast-1.compute.internal>
2022-09-21 07:53:19 +0000

    [Add] 商品登録、一覧、ジャンル機能

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,2 +1,9 @@
 class Item < ApplicationRecord
+  has_one_attached :image
+  belongs_to :genre
+
+
+  validates :name, presence: true
+  validates :detail, presence: true
+  validates :unit_price, presence: true
 end
