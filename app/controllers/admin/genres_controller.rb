ed308b31d88718c09bbb334cbda9912e5de230b9
EC2 Default User <ec2-user@ip-172-31-17-129.ap-northeast-1.compute.internal>
2022-09-23 04:55:33 +0000

    [Add] 商品編集バリデーション追加

--- app/controllers/admin/genres_controller.rb
+++ app/controllers/admin/genres_controller.rb
@@ -6,8 +6,11 @@ class Admin::GenresController < ApplicationController
 
   def create
     @genre = Genre.new(genre_params)
-    @genre.save
-    redirect_to admin_genres_path
+    if @genre.save
+      redirect_to admin_genres_path
+    else
+      render :index
+    end
   end
 
   def edit
@@ -17,11 +20,11 @@ class Admin::GenresController < ApplicationController
   def update
     @genre = Genre.find(params[:id])
     @genre.update(genre_params)
-    redirect_to admin_genres_path  
+    redirect_to admin_genres_path
   end
-  
+
   private
-  
+
   def genre_params
     params.require(:genre).permit(:name)
   end
