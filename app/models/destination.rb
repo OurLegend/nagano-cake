3b24367528c57f35e362232f619e9d0b0d5724b3
EC2 Default User <ec2-user@ip-172-31-43-22.ap-northeast-1.compute.internal>
2022-09-23 04:45:20 +0000

    [Add]注文機能追加

--- app/models/destination.rb
+++ app/models/destination.rb
@@ -4,4 +4,8 @@ class Destination < ApplicationRecord
     validates :shipping_postcode, presence: true
     validates :shipping_address, presence: true
     validates :direction, presence: true
+
+    def destination_all
+      self.shipping_postcode+" "+self.shipping_address+" "+self.direction
+    end
 end
