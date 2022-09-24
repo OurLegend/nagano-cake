bc9679c912e1ef7a2630f7c5ce5ca8bc819c4e7f
EC2 Default User <ec2-user@ip-172-31-35-7.ap-northeast-1.compute.internal>
2022-09-24 08:30:45 +0000

    仮commit

--- app/controllers/admin/orders_controller.rb
+++ app/controllers/admin/orders_controller.rb
@@ -1,14 +1,20 @@
 class Admin::OrdersController < ApplicationController
   def show
     @order = Order.find(params[:id])
+    @order_details = @order.order_details
+    @total = 0
+    
   end
 
   def update
     @order = Order.find(params[:id])
     @order.update(order_params)
+    @order_details = @order.order_details
     redirect_to admin_order_path
   end
   
+   private
+ 
   def order_params
     params.require(:order).permit(:payment_method, :shipping_address, :postage, :shipping_postcode, :shipping_name, :total_price, :status)
   end
