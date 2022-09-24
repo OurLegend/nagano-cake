66753108d5e6b2097ade676df332d5399af4dd9d
yooske <yooske0214@gmail.com>
2022-09-24 05:50:12 +0000

    [update] カート内商品の数量変更、削除、空にする機能の追加、その他設定を追加

--- app/controllers/public/cart_items_controller.rb
+++ app/controllers/public/cart_items_controller.rb
@@ -1,8 +1,23 @@
 class Public::CartItemsController < ApplicationController
   def create
-    @cart_item = CartItem.new(cart_item_params)
-    @cart_item.save
-    redirect_to public_cart_items_path
+    @cart_items = current_customer.cart_items.all
+    @cart_item = current_customer.cart_items.new(cart_item_params)
+    if @cart_item.amount != nil
+      @cart_items.each do |cart_item|
+        if cart_item.item_id == @cart_item.item_id
+          new_amount = cart_item.amount + @cart_item.amount
+          cart_item.update_attribute(:amount, new_amount)
+          @cart_item.delete
+        end
+      end
+      @cart_item.save
+      redirect_to public_cart_items_path
+    else
+      @item = @cart_item.item
+      @cart_item = CartItem.new
+      flash[:notice] = "個数を選択してください"
+      render "public/items/show"
+    end
   end
 
   def index
@@ -11,12 +26,21 @@ class Public::CartItemsController < ApplicationController
   end
 
   def update
+    @cart_item = CartItem.find(params[:id])
+    @cart_item.update(cart_item_params)
+    redirect_to public_cart_items_path
   end
 
   def destroy
+    @cart_item = CartItem.find(params[:id])
+    @cart_item.destroy
+    redirect_to public_cart_items_path
   end
 
   def destroy_all
+    current_customer.cart_items.destroy_all
+    redirect_to public_cart_items_path
+
   end
 
   def cart_item_params
