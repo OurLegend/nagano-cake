a79bf05986a3338f9595d9f65a0e1ad6bc23982d
EC2 Default User <ec2-user@ip-172-31-35-7.ap-northeast-1.compute.internal>
2022-09-25 02:41:45 +0000

    アクセス制限

--- app/controllers/public/items_controller.rb
+++ app/controllers/public/items_controller.rb
@@ -1,4 +1,6 @@
 class Public::ItemsController < ApplicationController
+   before_action :authenticate_customer!, if: :public_url, except: [:index, :show]    
+  
   def index
     @items = Item.page(params[:page]).per(8)
   end
@@ -6,5 +8,9 @@ class Public::ItemsController < ApplicationController
   def show
     @item = Item.find(params[:id])
     @cart_item = CartItem.new
+  end
+  
+  def admin_url
+    request.fullpath.include?("/admin")
   end
 end
